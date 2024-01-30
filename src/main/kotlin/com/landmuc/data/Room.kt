ac058a594eb5ae0f0ea383dde2c74d591108c22b
landmuc <landmucdev@gmail.com>
2024-01-30 21:40:52 +0100

    Implemented Phases

--- src/main/kotlin/com/landmuc/data/Room.kt
+++ src/main/kotlin/com/landmuc/data/Room.kt
@@ -8,6 +8,33 @@ class Room(
     val maxPlayers: Int,
     var players: List<Player> = listOf()
 ) {
+    private var phaseChangedListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYERS
+        set(value) {
+            // synchronized -> so only one thread at a time sets this var
+            synchronized(field) {
+                field = value
+                phaseChangedListener?.let { change ->
+                    change(value)
+                }
+            }
+        }
+    private fun setPhaseChangedListener(listener: (Phase) -> Unit) {
+        phaseChangedListener = listener
+    }
+
+    init {
+        setPhaseChangedListener { phase ->
+            when(phase) {
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_FOR_START -> waitingForStart()
+                Phase.NEW_ROUND -> newRound()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
+
     suspend fun broadcast(message: String) {
         players.forEach {player ->
             if (player.socket.isActive) {
@@ -27,5 +54,23 @@ class Room(
     fun containsPlayer(username: String): Boolean {
         return players.find { player ->
             player.username == username } != null
+    }
+
+    private fun waitingForPlayers() {}
+
+    private fun waitingForStart() {}
+
+    private fun newRound() {}
+
+    private fun gameRunning() {}
+
+    private fun showWord() {}
+
+    enum class Phase {
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
